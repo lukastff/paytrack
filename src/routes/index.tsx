a02a6e89a2dee0e950ee40032bb377d1c2344389
lukastff <lucastavaresffr@gmail.com>
2026-03-05 23:07:34 -0300

    started project

---
+++ src/routes/index.tsx
@@ -0,0 +1,39 @@
+import { BrowserRouter } from "react-router";
+
+import { Loading } from "../components/Loading";
+
+import { AuthRoutes } from "./AuthRoutes";
+import { ManagerRoutes } from "./ManagerRoutes";
+import { EmployeeRoutes } from "./EmployeeRoutes";
+
+const isLoading = false;
+// const session = undefined
+
+const session = {
+  user: {
+    role: "",
+  },
+};
+
+export function Routes() {
+  function Route() {
+    switch (session?.user.role) {
+      case "employee":
+        return <EmployeeRoutes />;
+      case "manager":
+        return <ManagerRoutes />;
+      default:
+        return <AuthRoutes />;
+    }
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <BrowserRouter>
+      <Route />
+    </BrowserRouter>
+  );
+}
