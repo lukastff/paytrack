62d96a56522a1cfc517c6f3572a03ab2c646cccf
lukastff <lucastavaresffr@gmail.com>
2026-03-10 23:06:42 -0300

    feat(auth): implement role-based routing for manager and employee

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,7 +2,8 @@ import { useState } from "react"
 import { createContext } from "react"
 
 type AuthContext = {
-  session: null | UserAPIResponse
+  session: null | UserAPIResponse,
+  save: (data: UserAPIResponse) => void
 }
 
 export const AuthContext = createContext({} as AuthContext )
@@ -10,8 +11,14 @@ export const AuthContext = createContext({} as AuthContext )
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [session, setSession] = useState<null | UserAPIResponse>(null)
 
+  function save(data: UserAPIResponse) {
+    setSession(data)
+  }
+
+  console.log(session)
+
   return (
-    <AuthContext.Provider value={{ session }}>
+    <AuthContext.Provider value={{ session, save }}>
       { children }
     </ AuthContext.Provider>
   )
