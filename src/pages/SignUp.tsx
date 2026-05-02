f5b230d5905da1c7f8078d8cac6a37c76e204ec3
lukastff <lucastavaresffr@gmail.com>
2026-05-02 20:13:02 -0300

    feat: implement authentication pages with form validation with zod

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,18 @@
 import { useState } from "react";
+import { z, ZodError } from "zod"
+
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+
+const signUpSchema = z.object({
+    name: z.string().trim().min(1, { message: "Informe o nome" }),
+    email: z.string().email({ message: "E-mail inválido" }),
+    password: z.string().min(6, { message: "Senha deve ter pelo menos 6 dígitos" }),
+    passwordConfirm: z.string({ message: "Confirme a senha" })
+}).refine((data) => data.password === data.passwordConfirm, {
+    message: "As senhas não são iguais",
+    path: ["passwordConfirm"]
+})
 
 export function SignUp() {
   const [name, setName] = useState("");
@@ -11,8 +23,24 @@ export function SignUp() {
 
   function onSubmit(e: React.FormEvent) {
     e.preventDefault();
+    try {
+        setIsLoading(true);
 
-    console.log(name, email, password, passwordConfirm);
+        const data = signUpSchema.parse({
+            name,
+            email,
+            password,
+            passwordConfirm,
+        })
+    } catch (error) {
+        if(error instanceof ZodError) {
+            return alert(error.issues[0].message)
+        }
+
+        alert("Não foi possível cadastrar!")
+    } finally {
+        setIsLoading(false);
+    }
   }
 
   return (
