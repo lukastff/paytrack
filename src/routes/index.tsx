728cbdb819de2e78ffeeff7193751ee44f6d5542
lukastff <lucastavaresffr@gmail.com>
2026-05-03 14:37:53 -0300

    feat(auth): implement AuthContext and useAuth hook

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter } from "react-router";
 
 import { Loading } from "../components/Loading";
 
+import { useAuth } from "../hooks/useAuth";
+
 import { AuthRoutes } from "./AuthRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
@@ -16,6 +18,9 @@ const session = {
 };
 
 export function Routes() {
+  const context = useAuth()
+  console.log(context)
+
   function Route() {
     switch (session?.user.role) {
       case "employee":
