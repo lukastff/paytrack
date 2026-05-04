2eb394c2e0e8dd1394c300e53f8dc59bce6f35e4
lukastff <lucastavaresffr@gmail.com>
2026-05-03 23:21:46 -0300

    feat(auth): implement session persistence and loading state

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,10 +8,8 @@ import { AuthRoutes } from "./AuthRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 
-const isLoading = false;
-
 export function Routes() {
-  const { session } = useAuth()
+  const { session, isLoading } = useAuth()
 
   function Route() {
     switch (session?.user.role) {
