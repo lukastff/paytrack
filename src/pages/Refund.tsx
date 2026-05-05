ebc811d6677c60a9f5dbcef9f0784cdd135e33c4
lukastff <lucastavaresffr@gmail.com>
2026-05-05 12:03:33 -0300

    feat(refund): implement fetching and viewing refund details

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import { useNavigate, useParams } from "react-router";
 import { z, ZodError } from "zod";
 import { AxiosError } from "axios";
@@ -12,6 +12,7 @@ import { Input } from "../components/Input";
 import { Select } from "../components/Select";
 import { Upload } from "../components/Upload";
 import { Button } from "../components/Button";
+import {formatCurrency} from "../utils/formatCurrency.ts";
 
 const refundSchema = z.object({
   name: z.string().min(3, { message: "Informe um nome claro para sua solicitação "}),
@@ -25,6 +26,7 @@ export function Refund() {
   const [category, setCategory] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [file, setFile] = useState<File | null>(null);
+  const [fileURL, setFileURL] = useState<string | null>(null)
 
   const navigate = useNavigate();
   const params = useParams<{ id: string }>();
@@ -75,6 +77,32 @@ export function Refund() {
     }
   }
 
+  async function fetchRefund(id: string) {
+    try {
+      const { data } = await api.get<RefundAPIResponse>(`/refunds/${id}`)
+
+      setName(data.name)
+      setCategory(data.category)
+      setAmount(formatCurrency(data.amount))
+      setFileURL(data.filename)
+
+    } catch(error) {
+      console.log(error)
+
+      if(error instanceof AxiosError) {
+        return alert(error.response?.data.message)
+      }
+
+      alert("Não foi possivel carregar")
+    }
+  }
+
+  useEffect(() => {
+    if(params.id) {
+      fetchRefund(params.id);
+    }
+  }, [params.id])
+
   return (
     <form
       onSubmit={onSubmit}
@@ -121,9 +149,9 @@ export function Refund() {
         />
       </div>
 
-      {params.id ? (
+      {params.id && fileURL ? (
         <a
-          href="https://www.rocketseat.com.br/"
+          href="https://www.google.com.br/"
           target="_black"
           className="text-sm text-green-100 font-semibold flex items-center justify-center gap-2 my-6 hover:opacity-70 transition ease-linear"
         >
